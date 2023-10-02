fb0cd9110508bea7968fe2bb56de483234b585d5
Sebastian D <me@panablay.com>
2023-10-03 00:25:33 +0800

    Dynamic offsetting
    No longer need to hardcode "-24(t4)" for flw, "loop_offset" resets the memory address of t4 to the initial one

--- Machine Project 1/mp1.asm
+++ Machine Project 1/mp1.asm
@@ -15,6 +15,11 @@
 	ecall 
 .end_macro
 
+.macro PRINT_INT
+	li a7, 1
+	ecall
+.end_macro
+
 .macro GET_FLOAT 
 	li a7, 6 
 	ecall  
@@ -30,7 +35,7 @@
 	input_column: .string "Column size:	"
 	elements: .string "Elements:	"
 	matrix: .space 1000
-	matrixspace: .string " "
+	matrixspace: .string "	"
 
 .text   
 	PRINT_STRING input_row
@@ -39,46 +44,56 @@
 	PRINT_STRING input_column
 	GET_INT
 	mv t1, a0
-	mul t2, t0, t1
 	
-	addi x1, x1, 0
+	mul t2, t0, t1 # matrix size, row (t0) * column (t1)
 	
 	# Initialize elements counter
 	li t3, 0
 	li x3, 0
+	li s3, 0
 	
 	# Load matrix address
 	la t4, matrix
-	# lw x4, (t4)
+	
+	# Matrix size * 4 bytes (interval)
+	li x4, 4
 	
 	PRINT_STRING elements
 	NEWLINE
 	
 	input_loop:
-		beq t3, t2, print_loop
+		beq t3, t2, loop_offset
 
 		GET_FLOAT
 		
 		fsw fa0, 0(t4)
 		
-		addi t4, t4, 4
+		add t4, t4, x4
 		addi t3, t3, 1
 		
 		j input_loop
 		
+	loop_offset:
+		beq s3, t3, print_loop
+		
+		sub t4, t4, x4
+		addi s3, s3, 1
+		
+		j loop_offset
+		
 	print_loop:
 		beq x3, t2, done
 		
-		addi x4, x3, 1
-		rem x5, x4, t1
+		addi x1, x3, 1
+		rem x2, x1, t1
 		
-		flw fa0, -24(t4)
+		flw fa0, 0(t4)
 		PRINT_FLOAT
 		
-		addi t4, t4, 4
+		add t4, t4, x4
 		addi x3, x3, 1
 		
-		beq x5, zero, print_newline
+		beq x2, zero, print_newline
 		PRINT_STRING matrixspace
 		
 		j print_loop
